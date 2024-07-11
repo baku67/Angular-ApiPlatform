e0b958f3fb6e60150379d88f8e1710033252f65c
baku67 <basile08@hotmail.fr>
2024-07-11 03:08:06 +0200

    RAS app.routes.ts

--- ~ Angular-tuto-officiel/src/app/app.routes.ts
+++ ~ Angular-tuto-officiel/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
-export const routes: Routes = [{ path: 'projects/:id', component: ProjectDetailsComponent },];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectListComponent },
+    { path: 'projects/:id', component: ProjectDetailsComponent },
+    { path: '', redirectTo: '/projects', pathMatch: 'full' },
+    { path: '**', redirectTo: '/projects' }
+];
