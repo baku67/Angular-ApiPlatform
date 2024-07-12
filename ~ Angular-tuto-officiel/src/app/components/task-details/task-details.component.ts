bf75b294b785723a54c57317ab12ded50e65a8ff
baku67 <basile08@hotmail.fr>
2024-07-12 20:04:12 +0200

    WIP errors et loading API (taskDetails)

--- ~ Angular-tuto-officiel/src/app/components/task-details/task-details.component.ts
+++ ~ Angular-tuto-officiel/src/app/components/task-details/task-details.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
-export class TaskDetailsComponent {
+export class TaskDetailsComponent implements OnInit {
+
+  task: any = null;
+  errors: any = null;
+  isLoading: boolean = false; // Déclaration de la variable isLoading
+
+  constructor(private route: ActivatedRoute, private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    // Retrieve the project ID from the route parameters
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(`Route ID: ${id}`); // Log the ID retrieved from the route
+    if (id) {
+
+      this.isLoading = true; // Activer l'indicateur de chargement
+      this.taskService.getTask(id).subscribe({
+        next: data => {
+
+          console.log('Service response:', data); // Log the entire response
+          this.task = data;
+          console.log('Task:', JSON.stringify(this.task));
+          this.isLoading = false; // Désactiver l'indicateur de chargement lorsque la requête est terminée
+        },
+        error: err => {
+          this.errors = err;
+          console.error('Error fetching user:', err);
+          this.isLoading = false; // Désactiver l'indicateur de chargement lorsque la requête est terminée
+        },
+        complete: () => {
+          // this.isLoading = false;  // Désactiver l'indicateur de chargement lorsque la requête est terminée
+        }
+      });
+    } else {
+      console.error('No task ID found in route parameters');
+    }
+  }
+
 
 }
--- ~ Angular-tuto-officiel/src/app/components/user-details/user-details.component.ts
+++ ~ Angular-tuto-officiel/src/app/components/user-details/user-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { ProjectService } from '../../services/project.service';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { RouterLink } from '@angular/router';
@@ -15,14 +14,18 @@ import { RouterLink } from '@angular/router';
 export class UserDetailsComponent implements OnInit {
 
   user: any = null;
+  errors: any = null;
+  isLoading: boolean = false; // Déclaration de la variable isLoading
 
-  constructor(private route: ActivatedRoute, private userService: UserService, private projectService: ProjectService) { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
     // Retrieve the project ID from the route parameters
     const id = this.route.snapshot.paramMap.get('id');
     console.log(`Route ID: ${id}`); // Log the ID retrieved from the route
     if (id) {
+
+      this.isLoading = true; // Activer l'indicateur de chargement
       this.userService.getUser(id).subscribe({
         next: data => {
 
@@ -31,14 +34,14 @@ export class UserDetailsComponent implements OnInit {
           console.log('User:', JSON.stringify(this.user));
         },
         error: err => {
+          this.errors = err;
           console.error('Error fetching user:', err);
-        }
+        },
+
       });
     } else {
       console.error('No user ID found in route parameters');
     }
-
-
   }
 
 }
