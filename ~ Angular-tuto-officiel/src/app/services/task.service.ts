e93d4b4410412acc33c34c700ceee6eda4439110
baku67 <basile08@hotmail.fr>
2024-07-16 01:00:01 +0200

    RAS

--- ~ Angular-tuto-officiel/src/app/services/task.service.ts
+++ ~ Angular-tuto-officiel/src/app/services/task.service.ts
@@ -18,4 +18,19 @@ export class TaskService {
   getTask(taskId: any): Observable<any> {
     return this.http.get(`${this.apiUrl}/${taskId}`);
   }
+
+  // createTask(task: Task): Observable<Task> {
+  //   return this.http.post<Task>(this.apiUrl, task);
+  // }
+
+  // updateTask(task: Task): Observable<Task> {
+  //   return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  // }
+
+  // deleteTask(id: number): Observable<void> {
+  //   return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  // }
+
+
 }
+
