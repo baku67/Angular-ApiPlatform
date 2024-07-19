e22ab2b11cd1e8769a01801b0602ccb291e443c9
baku67 <basile08@hotmail.fr>
2024-07-19 21:32:41 +0200

    WIP umlShape

--- ~ Angular-tuto-officiel/src/app/components/diagram-palet/diagram-palet.component.ts
+++ ~ Angular-tuto-officiel/src/app/components/diagram-palet/diagram-palet.component.ts
@@ -124,6 +124,144 @@ export class DiagramPaletComponent implements OnInit {
         ];
         return basicShapes;
     };
+    public getUmlShapes(): NodeModel[] {
+        let umlShapes: NodeModel[] = [
+            {
+                id: 'Action',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'Action'
+                }
+            },
+            {
+                id: 'Decision',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'Decision'
+                }
+            },
+            {
+                id: 'MergeNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'MergeNode'
+                }
+            },
+            {
+                id: 'InitialNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'InitialNode'
+                }
+            },
+            {
+                id: 'FinalNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'FinalNode'
+                }
+            },
+            {
+                id: 'ForkNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'ForkNode'
+                }
+            },
+            {
+                id: 'JoinNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'JoinNode'
+                }
+            },
+            {
+                id: 'TimeEvent',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'TimeEvent'
+                }
+            },
+            {
+                id: 'AcceptingEvent',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'AcceptingEvent'
+                }
+            },
+            {
+                id: 'SendSignal',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'SendSignal'
+                }
+            },
+            {
+                id: 'ReceiveSignal',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'ReceiveSignal'
+                }
+            },
+            {
+                id: 'StructuredNode',
+                shape: {
+                    type: 'UmlActivity',
+                    shape: 'StructuredNode'
+                }
+            },
+            {
+                id: 'class',
+                style: {
+                    fill: '#26A0DA',
+                },
+                borderColor: 'white',
+                shape: {
+                    type: 'UmlClassifier',
+                    classShape: {
+                        attributes: [
+                            { name: 'accepted', type: 'Date', style: { color: "red", fontFamily: "Arial", textDecoration: 'Underline',  italic: true },isSeparator: true },
+                        ],
+                        methods: [{ name: 'getHistory', style: {}, parameters: [{ name: 'Date', style: {} }], type: 'History' }],
+                        name: 'Patient'
+                    },
+                    classifier: 'Class'
+                },
+            },
+            {
+                id: 'Interface',
+                style: {
+                    fill: '#26A0DA',
+                }, borderColor: 'white',
+                shape: {
+                    type: 'UmlClassifier',
+                    interfaceShape: {
+                        name: "Bank Account",
+                    },
+                    classifier: 'Interface'
+                },
+            },
+            {
+                id: 'Enumeration',
+                style: {
+                    fill: '#26A0DA',
+                }, borderColor: 'white',
+                shape: {
+                    type: 'UmlClassifier',
+                    enumerationShape: {
+                        name: 'AccountType',
+                        members: [
+                            {
+                                name: 'Checking Account', style: {}
+                            },
+                        ]
+                    },
+                    classifier: 'Enumeration'
+                },
+            },
+        ];
+        return umlShapes;
+    }
     public getFlowShapes(): NodeModel[] {
         let flowShapes: NodeModel[] = [{
                 id: 'Terminator',
@@ -322,7 +460,7 @@ export class DiagramPaletComponent implements OnInit {
               //Sets the id of the palette
               id: 'flow',
               //Sets whether the palette expands/collapse its children
-              expanded: true,
+              expanded: false,
               //Adds the palette items to palette
               symbols: this.getFlowShapes(),
               //Sets the header text of the palette
@@ -330,15 +468,22 @@ export class DiagramPaletComponent implements OnInit {
               iconCss: 'e-ddb-icons e-flow'
           },
           {
+            id: 'uml',
+            expanded: true,
+            symbols: this.getUmlShapes(),
+            title: 'Formes UML',
+            iconCss: 'e-ddb-icons e-uml'
+          },
+          {
               id: 'basic',
-              expanded: true,
+              expanded: false,
               symbols: this.getBasicShapes(),
               title: 'Formes basiques',
               iconCss: 'e-ddb-icons e-basic'
           },
           {
               id: 'connectors',
-              expanded: true,
+              expanded: false,
               symbols: this.getConnectors(),
               title: 'Connecteurs',
               iconCss: 'e-ddb-icons e-connector'
